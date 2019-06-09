c7c4949bdbe00cb759a41bf8d7c6f4ddfe704213
WenKai Chen <42525310+turbohiro@users.noreply.github.com>
2019-06-09 15:11:36 +0800

    Update resnet.py

--- resnet.py
+++ resnet.py
@@ -13,7 +13,7 @@ os.environ['CUDA_VISIBLE_DEVICES']='0'
 dataDir = '/data/jupyter/libin713/sample_IDCard'
 
 #1)读取tfrecords数据训练集
-def read_and_decode(filename):  # 读入dog_train.tfrecords
+def read_and_decode(filename):  # 读入tfrecords
     filename_queue = tf.train.string_input_producer([filename])  # 生成一个queue队列
     reader = tf.TFRecordReader()
     _, serialized_example = reader.read(filename_queue)  # 返回文件名和文件
@@ -24,7 +24,7 @@ def read_and_decode(filename):  # 读入dog_train.tfrecords
                                        })  # 将image数据和label取出来
  
     img = tf.decode_raw(features['img_raw'], tf.uint8)
-    img = tf.reshape(img, [600, 600, 3])  # reshape为128*128的3通道图
+    img = tf.reshape(img, [600, 600, 3])  # reshape为600*600的3通道图
     img = tf.cast(img, tf.float32) * (1. / 255) - 0.5  # 在流中抛出img张量
     label = tf.cast(features['label'], tf.int32)  # 在流中抛出label张量
     return img,label
